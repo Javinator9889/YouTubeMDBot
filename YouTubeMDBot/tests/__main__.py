530952fc676b3071da2a3f6c6ccd7f89be5191dd
Javinator9889 <javialonso007@hotmail.es>
2019-09-20 16:15:46 +0200

    GitLab CI
    Signed-off-by: Javinator9889 <javialonso007@hotmail.es>

--- YouTubeMDBot/tests/__main__.py
+++
@@ -1,4 +0,0 @@
-from .downloader import DownloadTest
-
-if __name__ == '__main__':
-    DownloadTest.test_multithread_download()
--- YouTubeMDBot/tests/downloader.py
+++ YouTubeMDBot/tests/downloader.py
@@ -1,10 +1,15 @@
 import threading
 import unittest
+from time import sleep
 
 from YouTubeMDBot.downloader import YouTubeDownloader
 
 
 class DownloadTest(unittest.TestCase):
+    _elements = 0
+    _max = 0
+    _lock = threading.Lock()
+
     def test_multithread_download(self):
         yt1 = YouTubeDownloader(url="https://www.youtube.com/watch?v=Inm-N5rLUSI")
         yt2 = YouTubeDownloader(url="https://www.youtube.com/watch?v=-_ZwpOdXXcA")
@@ -15,17 +20,26 @@ class DownloadTest(unittest.TestCase):
         t3 = threading.Thread(target=self.write_to_file, args=(yt3, "v3.m4a",))
         t4 = threading.Thread(target=self.write_to_file, args=(yt4, "v4.m4a",))
 
+        self._max = 4
+
         t1.start()
         t2.start()
         t3.start()
         t4.start()
 
-    @staticmethod
-    def write_to_file(yt: YouTubeDownloader, name: str):
+        while self._elements < self._max:
+            sleep(1)
+
+    def barrier(self):
+        with self._lock:
+            self._elements += 1
+
+    def write_to_file(self, yt: YouTubeDownloader, name: str):
         _, data = yt.download()
         print(name + " downloaded")
         with open(name, "wb") as f:
             f.write(data)
+        self.barrier()
 
 
 if __name__ == '__main__':
